c6e1350b78adfcf07b0f1613c70894fef5bcd159
migo-robben <wuguohui@winsing.net>
2021-04-05 12:30:50 +0800

    10_OpenGLIBLIrradiance optimize note

--- src/Shaders/IrradianceConvolution.fs.glsl
+++ src/Shaders/IrradianceConvolution.fs.glsl
@@ -15,8 +15,8 @@ void main() {
     vec3 irradiance = vec3(0.0);
 
     vec3 up    = vec3(0.0, 1.0, 0.0);
-    vec3 right = cross(up, N);
-    up         = cross(N, right);
+    vec3 right = normalize(cross(up, N));
+    up         = normalize(cross(N, right));
 
     float sampleDelta = 0.025;
     float nrSamples = 0.0;
@@ -26,12 +26,14 @@ void main() {
         for (float theta = 0.0; theta < 0.5 * PI; theta += sampleDelta)
         {
             vec3 tangentSample = vec3(sin(theta) * cos(phi),  sin(theta) * sin(phi), cos(theta));
+
             vec3 sampleVec = mat3(right, up, N) * tangentSample;
+            // vec3 sampleVec = tangentSample.x * right + tangentSample.y * up + tangentSample.z * N;
+
             irradiance += textureCube(environmentMap, sampleVec).rgb * cos(theta) * sin(theta);
             nrSamples++;
         }
     }
     irradiance = PI * irradiance * (1.0 / float(nrSamples));
     FragColor = vec4(irradiance, 1.0);
-
 }
